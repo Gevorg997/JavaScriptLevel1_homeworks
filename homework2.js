a20b6a3f8035d28b4f4d74d8bb8d234feda7c41d
Gevorg997 <99825517+Gevorg997@users.noreply.github.com>
2022-04-21 15:35:58 +0400

    Create homework2.js

---
+++ homework2.js
@@ -0,0 +1,29 @@
+//1. Given an array of numbers which is almost sorted in ascending order.  Find the index where sorting order is violated.
+
+function findViolatingIndex (arr){
+    let wrongNum = arr.find(function (el, i , ar){
+                if(ar[i] < ar[i-1]){
+                 return true
+                }
+              })
+    return arr.indexOf(wrongNum)
+}
+
+//2. Given an array consisting from the arrays of numbers (like a two-dimensional array). Find sum of each row and print them as an array
+
+function elSum (arr) {
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){       
+        sum += arr[i]
+    }
+    return sum
+}
+
+function arrSum(arr){
+    let result = []
+    for(let i = 0; i < arr.length; i++){
+    result.push(elSum(arr[i]))
+    }
+    return result
+} 
+
