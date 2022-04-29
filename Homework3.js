81c7b72200df0889c1ea6ccd4b0427d3c9cd5e3c
Gevorg997 <99825517+Gevorg997@users.noreply.github.com>
2022-04-29 23:54:35 +0400

    Update Homework3.js

--- Homework3.js
+++ Homework3.js
@@ -30,3 +30,33 @@ function flatten(arr) {
     }
     return newArr
 }
+
+
+//4. 	Given the list of the following readers:
+//Output the books sorted by the percent in descending order which readStatus is true.
+// [
+//	{ book: "Catcher in the Rye", readStatus: true, percent: 40},
+//	{ book: "Animal Farm", readStatus: true, percent: 20},
+//	{ book: "Solaris", readStatus: false, percent: 90 },
+//	{ book: "The Fall", readStatus: true, percent: 50 },
+//	{ book: "White Nights", readStatus: false, percent: 60 } ,
+//	{ book: "After Dark", readStatus: true, percent: 70 }
+];
+
+function sortedList(list) {
+   return   list
+                .filter(el => el.readStatus)
+                .sort((a,b) => b.percent - a.percent)
+}
+
+
+
+
+
+
+
+
+
+
+
+
